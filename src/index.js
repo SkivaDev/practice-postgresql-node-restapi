f2717c5b42f22b996ca0f6185e250bdf2cfa83bc
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-24 12:03:06 -0500

    feat(project): :sparkles: add files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import express from "express";
+import { PORT } from "./config";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
