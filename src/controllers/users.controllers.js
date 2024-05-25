029d823fa0fb48d28f4560776a7450dc442d40c7
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-05-25 18:44:13 -0500

    feat(project): :sparkles: Controllers

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,55 @@
+import { pool } from "../db.js";
+
+export const getUsers = async (req, res) => {
+    const { rows } = await pool.query("SELECT * FROM users;");
+    res.json(rows);
+  }
+
+export const getUserById =  async (req, res) => {
+    const { id } = req.params;
+    
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1;", [id]);
+
+    if (rows.length === 0) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(rows);
+}
+
+export const createUser = async (req, res) => {
+
+    try {
+        const { name, email } = req.body;
+        const { rows } = await pool.query("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *;", [name, email]);
+        res.json(rows);
+    } catch (error) {
+        if (error?.code === "23505") {
+            return res.status(409).json({ message: "Email already exists" });
+        }
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    const { rows, rowCount } = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *;", [id]);
+
+    if (rowCount === 0) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(rows);
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const { rows, rowCount } = await pool.query("UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *;", [name, email, id]);
+
+    if (rowCount === 0) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(rows);
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,51 +1,17 @@
 import { Router } from "express";
-import { pool } from "../db.js";
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from "../controllers/users.controllers.js";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", getUsers);
 
-  const { rows } = await pool.query("SELECT * FROM users;");
-  res.json(rows);
-});
-router.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1;", [id]);
+router.get("/:id",getUserById);
 
-    if (rows.length === 0) {
-        return res.status(404).json({ message: "User not found" });
-    }
+router.post("/", createUser);
 
-    res.json(rows);
-});
-router.post("/", async (req, res) => {
+router.delete("/:id", deleteUser);
 
-    const { name, email } = req.body;
-    const { rows } = await pool.query("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *;", [name, email]);
-    res.json(rows);
-});
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { rows, rowCount } = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *;", [id]);
-
-    if (rowCount === 0) {
-        return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(rows);
-});
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { name, email } = req.body;
-    const { rows, rowCount } = await pool.query("UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *;", [name, email, id]);
-
-    if (rowCount === 0) {
-        return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(rows);
-});
+router.put("/:id", updateUser);
 
 
 export default router;
